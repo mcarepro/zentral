88d044a5f66214e0258c97f845e9770cc91851f0
Éric Falconnier <eric.falconnier@112hz.com>
2017-02-03 21:26:02 +0100

    Add nagios app

---
+++ zentral/contrib/nagios/apps.py
@@ -0,0 +1,6 @@
+from zentral.utils.apps import ZentralAppConfig
+
+
+class ZentralNagiosAppConfig(ZentralAppConfig):
+    name = "zentral.contrib.nagios"
+    verbose_name = "Zentral Nagios contrib app"
